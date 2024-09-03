48581234b31f6d9f98b09244a6e703e68c68126c
Hash-Ezz <ezz.alhashedi@gmail.com>
2024-09-03 06:35:45 +0300

    add

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {username , city } from "../data/user";
+
+function Home() {
+  return (
+    <div id="home">u
+      <h1>
+        {username} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
+}
+
+
+export default Home;
